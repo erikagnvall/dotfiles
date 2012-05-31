0e1886010c132aaef9fe6a8ee5a9d29ba514474e
Erik Jansson <erikjansson90@gmail.com>
2012-06-01 00:14:55 +0200

    More advanced bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,18 @@
-mkdir bundle
+PLUGIN_FILE=plugins
+BUNDLE_DIR=bundle
 
-cd bundle
+if [ ! -e "$PLUGIN_FILE" ]; then
+	echo "No plugin file found. Exiting"
+	exit 1
+fi
 
-while read line        
-do        
-	git clone $line
-done <../plugins
+if [ ! -d "$BUNDLE_DIR" ]; then
+	mkdir $BUNDLE_DIR
+fi
+
+while read url
+do
+	name=`echo $url | cut -d '/' -f 5- | sed 's/\(.*\)..../\1/'`
+
+	git clone $url $BUNDLE_DIR/$name
+done < $PLUGIN_FILE
