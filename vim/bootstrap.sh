65f484d257305547575b64df8a912cab74ebea84
Erik Jansson <erikjansson90@gmail.com>
2012-06-30 17:18:54 +0200

    Added special mutt.vim script

--- vim/bootstrap.sh
+++ vim/bootstrap.sh
@@ -10,6 +10,14 @@ if [ ! -d "$BUNDLE_DIR" ]; then
 	mkdir $BUNDLE_DIR
 fi
 
+# Special handling of mutt.vim
+if [ ! -d "$BUNDLE_DIR/mutt/" ]; then
+	mkdir -p "$BUNDLE_DIR/mutt/ftplugin/mail"
+	cp mutt.vim "$BUNDLE_DIR/mutt/ftplugin/mail/"
+	echo "Installed mutt.vim"
+fi
+
+
 while read url
 do
 	name=`echo $url | cut -d '/' -f 5- | sed 's/\(.*\)..../\1/'`
